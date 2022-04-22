3f259e2dc2c324b5300d89284d8cfc2d6f97cac9
Wiktor Augustyniak <wiktor.augustyniak@gmail.com>
2022-04-22 17:10:45 +0200

    add function wich check player choice

--- main.js
+++ main.js
@@ -18,3 +18,16 @@ const summary = {
     loses: 0,
     draws: 0,
 };
+
+// function checking players choice and updating battle status in left-panel
+
+function playerHand() {
+
+    hands.forEach(hand => hand.style.boxShadow = '');
+    this.style.boxShadow = '0 10px 5px -5px black';
+    battle.playerChoice = this.dataset.option;
+
+    console.log(battle.playerChoice);
+};
+
+hands.forEach(hand => hand.addEventListener('click', playerHand));
