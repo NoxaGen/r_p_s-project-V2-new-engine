50649ee783021b7939db0f589c80ac3093603fc3
Wiktor Augustyniak <wiktor.augustyniak@gmail.com>
2022-04-22 14:55:11 +0200

    create some objects and array in js

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
-const choices = {
-    playerChoice: '',
+const hands = [...document.querySelectorAll('.hands img')];
+const play = document.querySelector('button');
 
+const choices = {
+    player: '',
+    ai: '',
+};
+
+const battle = {
+    playerChoice: '',
+    aiChoice: '',
+    winner: '',
+};
+
+const summary = {
+    games: 0,
+    wins: 0,
+    loses: 0,
+    draws: 0,
 };
