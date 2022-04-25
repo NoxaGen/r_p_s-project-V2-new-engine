71c9a64f0cdab56ea6cfa8bad37084d35c9029c0
Wiktor Augustyniak <wiktor.augustyniak@gmail.com>
2022-04-25 17:34:06 +0200

    create function wich comparing two choices and return result

--- test.js
+++ test.js
@@ -4,13 +4,14 @@ const play = document.querySelector('button');
 const battle = {
     playerChoice: '',
     aiChoice: '',
+    result: ''
 };
 
 const summary = {
     games: 0,
     wins: 0,
     loses: 0,
-    draws: 0,
+    draws: 0
 };
 
 //Funkcja odpowiadająca za wybór gracza, podświetlenie wyboru oraz przekazanie informacji do obiektu
@@ -20,7 +21,7 @@ function playerHand() {
     hands.forEach(hand => {
         hand.addEventListener('click', function () {
             hand.style.boxShadow = "";
-            console.log(this.dataset.option);
+            console.log('wybór gracza:' + this.dataset.option);
 
             hands.forEach(hand => {
                 hand.style.boxShadow = "";
@@ -36,9 +37,20 @@ function playerHand() {
 function aiHand() {
     const ai = hands[Math.floor(Math.random() * 3)].dataset.option;
     battle.aiChoice = ai;
+    console.log('wybór AI:' + ai)
 }
 
+// Funkcja która porównuje wybór gracza z wyborem AI i zwraca wynik w formie stringa
 
+function checkResult(player, ai) {
+    if ((player === 'rock' && ai === 'rock') || (player === 'paper' && ai === 'paper') || (player === 'scissors' && ai === 'scissors')) {
+        return 'draw';
+    } else if ((player === 'rock' && ai === 'paper') || (player === 'paper' && ai === 'scissors') || (player === 'scissors' && ai === 'rock')) {
+        return 'loss';
+    } else {
+        return 'win';
+    }
+}
 
 
 //Funkcja sterująca programem w której umieszczone będą pokolei wszystkie funkcje
@@ -49,7 +61,10 @@ function aiHand() {
 function gameEngine() {
     playerHand()
     aiHand()
-}
 
+    battle.result = checkResult(battle.playerChoice, battle.aiChoice)
+    console.log(battle.result)
+
+}
 
 gameEngine()
